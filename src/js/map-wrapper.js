aed866a22ebd555d5eb8d69c8b01a77d72696ade
James M Pierce <mr.jpierce@gmail.com>
2017-05-28 14:06:33 -0600

    Finished styling. Added updateUI method.

--- src/js/map-wrapper.js
+++ src/js/map-wrapper.js
@@ -13,7 +13,6 @@ class MapWrapper {
             map: this.map,
             position: this.startingLocation
         });
-        
 
         // Attach to marker event
         google.maps.event.addListener(this.marker, 'dragend', () => {
@@ -23,27 +22,49 @@ class MapWrapper {
     }
 
     updateWeatherInformation(lat, lng) {
+        this.updateUIState('loading');
         let response = this.weatherService.getWeatherByGeo(lat, lng)
             .done((response) => {
-                $('#weather').show();
-                $('#api-error').hide();
-
+                console.log(response);
                 // Set data
-                let data = response.data,
+                let data = response,
                     sunrise = moment.unix(+data.sys.sunrise),
                     sunset = moment.unix(+data.sys.sunset);
-                $('#location').html(response.data.name);
+                $('#location').html(response.name);
                 $('#condition').html(data.weather[0].description);
                 $('#temperature').html(data.main.temp + ' F');
-                $('#humiditiy').html(data.main.humidity + '%');
+                $('#humidity').html(data.main.humidity + '%');
                 $('#sunrise').html(sunrise.format('HH:mm'));
                 $('#sunset').html(sunset.format('HH:mm'));
                 $('#wind-speed').html(data.wind.speed + ' mph');
                 $('#wind-direction').html(data.wind.deg + ' degrees');
+
+                this.updateUIState('success');
             })
             .fail(() => {
-                $('#weather').hide();
-                $('#api-error').show();
+                this.updateUIState('failure');
             });
+    }
+
+    updateUIState(state) {
+        switch(state) {
+            case 'loading':
+                $('#weather-loading').show();
+                $('#weather-success').hide();
+                $('#weather-error').hide();
+                break;
+
+            case 'success':
+                $('#weather-loading').hide();
+                $('#weather-success').show();
+                $('#weather-error').hide();
+                break;
+
+            case 'failure':
+                $('#weather-loading').hide();
+                $('#weather-success').hide();
+                $('#weather-error').show();
+                break;
+        }
     }
 }
